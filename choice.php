749a47619b1e93d4f7b17cbcb6b99d33db4586f8
Mandeep Singh <beniwalmandeep315@gmail.com>
2016-01-14 22:33:52 +0530

    Ext CSS, include
    -Css property changes
    -Changed error msg while redirecting to login

--- choice.php
+++ choice.php
@@ -1,50 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Welcome!</title>
+<title>Subscribe!</title>
+
+<link rel="stylesheet" type="text/css" href="style.css" />
 <style type="text/css">
 
-body {
-	font-family:sans-serif;
-}
-
-#Top
-{
-	width: 120%;
-	height: 100px;
-	margin: -10px;
-	background-color: #000000;
-	color: white;
-}
-
-#TopText
-{
-	padding-top:30px;
-}
-
-div div div{
-	border: 2px solid pink;
-	width: 95%;
-	float: left;
-	margin-left: 30px;
-	margin-top: 30px;
-	position: relative;
-	padding-top: 6%;
-	text-align: center;
-	font-size: 35px;
-	font-weight: bold;
-	color: blue;
-}
-
- #categories {
+#categories {
 	clear: both;
 	margin-top: 30px;
 	border: 2px solid red;
+	width: 96%;
+	margin-left: 2%;
+	height: 900px;
+}
+
+.subcategory-block{
+	border: 2px solid #2196F3;
 	width: 95%;
+	float: left;
+	margin: 2%;
+	position: relative;
+	padding-top: 6%;
+	padding-bottom: 1%;
+	text-align: center;
+	font-size: 2.3em;
+	font-weight: bold;
+	color: #2196F3;
 }
 
 #extra-stuff {
-	font-size: 20px;
+	font-size: 0.6em;
 	color: black;
 	font-weight: bold;
 	display: inline;
@@ -54,18 +40,16 @@ div div div{
 </head>
 
 <body>
-	<div id="Top">
-		<div id="TopText"><span style="font-size: 35px; padding-left:430px; padding-top:200px;"><strong>WELCOME TO MMI EXOTEL!</strong></span></div>
-	</div>
+	<?php include 'top_bar.php' ?>
 	
 	<div>
-	<br>
-	<a href="logout.php">LogOut</a>
+	<br/>
+	<a style="float:right" href="logout.php">LogOut</a>
 	<div id="categories">
 	<?php 
 	session_start();
 	if(!($u= $_SESSION["user"]))
-		header("location:index.php");
+		header("location:index.php?Please%20Login");
 	$cat = $_GET['cat'];
 	//echo $cat;
 	$servername = "localhost";
@@ -80,33 +64,32 @@ div div div{
 	for($i=0; $i<$count; $i++)
 	{
 		?>
-		<div>
+		<div class="subcategory-block">
 			<?php 
 			$data = mysql_fetch_row($res);
 			echo strtoupper($data[2]);
-			$res2=mysql_query("select Name,C.CompId from Cat_Comp CC,Company C where C.CompId=CC.CompId and SCat_Id='$data[1]'");
+			$res2=mysql_query("select Name, C.CompId from Cat_Comp CC, Company C where C.CompId=CC.CompId and SCat_Id='$data[1]'");
 			$cnt2=mysql_num_rows($res2);
-			$act = "submit.php?";
 			?>
-			<form action=submit.php method="get">
-			<input type="hidden" name="scat" value="<?php echo $data[1]; ?>">
-			<?php
-			for($j=0;$j<$cnt2;$j++)
-			{
-				$rowd=mysql_fetch_row($res2);
-				echo "<input type=checkbox name=Company[] value=".$rowd[1].">&nbsp;".$rowd[0]."       ";
-			?>
-			<span id="extra-stuff">&nbsp;&nbsp;&nbsp;&nbsp;low <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=0 />
-			&nbsp;&nbsp;&nbsp;&nbsp;medium <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=1 />
-			&nbsp;&nbsp;&nbsp;&nbsp;high <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=2 /> 
+			<form action="submit.php" method="get">
+				<input type="hidden" name="scat" value="<?php echo $data[1]; ?>">
+				<?php
+				for($j=0;$j<$cnt2;$j++)
+				{
+					$rowd=mysql_fetch_row($res2);
+					echo "<input type=checkbox name=Company[] value=rowd[1] />&nbsp;".$rowd[0]."       ";
+				?>
+				<span id="extra-stuff">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Low <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=0 required />
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Medium <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=1 />
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;High <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=2 /> 
 
-			&nbsp;&nbsp;&nbsp;&nbsp;Enter limit: <input style="width:50px" type="number" name="<?php echo $rowd[1]."limit"?>" />
-			<br/>
-			</span><br/>
-			<?php
-			}
-			?>
-			<input type=submit value="Subscribe!"/>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter Limit: <input style="width:50px" type="number" min=0 name="<?php echo $rowd[1]."limit"?>" required />
+				<br/>
+				</span>
+				<?php
+				}
+				?>
+				<input type=submit value="Subscribe!"/><br/>
 			</form>
 		
 		</div>

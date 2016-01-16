96c7bbb682000a99ceea3b58527d19ce78ba3fa2
Ib <mohammedibrahim219@yahoo.in>
2016-01-16 09:19:25 +0530

    Ported to mysqli from mysql

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 
 <link rel="stylesheet" type="text/css" href="style.css" />
 <style type="text/css">
-
 #logintab {
 font-size:1.2em;
 border-radius:5px;
@@ -15,25 +14,21 @@ background-color:#E8E8E8;
 width: 23%;
 margin-top: 150px;
 }
-
 #logintop {
 padding: 10px;
 border-radius: 5px 5px 0px 0px;
 border-bottom: 1px solid rgb(170, 170, 170);
 background-color: rgb(205, 205, 205);
 }
-
 .inputfield {
 margin: 25px 15px 0px 15px;
 }
-
 #loginbutton {
 border: 1px solid rgb(119, 119, 119);
 padding: 5px 12px;
 border-radius: 5px;
 background-color: rgb(221, 221, 221);
 }
-
 </style>
 </head>
 

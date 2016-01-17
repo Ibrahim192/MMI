14b4549e0d7d8ae76adf0ae4fd2fa11956019a04
Ibrahim192 <mohammedibrahim219@yahoo.in>
2016-01-17 14:39:00 +0530

    stuff

--- submit.php
+++ submit.php
@@ -4,9 +4,6 @@
 	$password = "";
 	$db="Mmi";
 	$conn = mysqli_connect($servername, $username,$password,$db);
-	$conn = mysql_connect($servername, $username,$password);
-	mysql_set_charset('utf8',$conn);
-	mysql_select_db('Mmi');
 	foreach($_GET['Company'] as $selected){
 		$scat=$_GET['scat'];
 		if (!isset($_SESSION["user"]))
@@ -17,10 +14,6 @@
 		$query="Select Name from SubCat where SubCat_id='$scat'";
 		$res2=mysqli_query($conn,$query);
 		$data2=mysqli_fetch_row($res2);
-		$res1=mysql_query("Select Name from Company where CompId='$selected'");
-		$data1=mysql_fetch_row($res1);
-		$res2=mysql_query("Select Name from SubCat where SubCat_id='$scat'");
-		$data2=mysql_fetch_row($res2);
 
 		echo "Successfully subscribed to <strong>".$data1[0]."</strong> for <strong>".$data2[0]."</strong>!! :)";
 		$pri = $_GET[$selected."priority"];

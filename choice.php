0ec481e8ce9702feeb731a7d10438869c3d658cc
Mandeep Singh <beniwalmandeep315@gmail.com>
2016-01-23 14:35:11 +0530

    Same page subscription feature
    deleted submit.php and made it in same page

--- choice.php
+++ choice.php
@@ -11,42 +11,33 @@
 	{
 		redirect("index.php?error_msg=Plese%20Log%20In%20first.");
 	}
+	if (isset($_POST["submit"]))
+	{
+		$pri = $_POST["pri"];
+		$lim =$_POST["lim"];
+		$compid = $_POST["compid"];
+		$catid = $_POST["catid"];
+		$compname = $_POST["compname"];
+		$catname = $_POST["catname"];
+		if ($_POST["submit"] == "Subscribe!")
+		{
+			$_SESSION["subs_msg"] = "You have been successfully subscribed to <b> ".$compname." </b> for <b>".$catname."</b>";
+			$subs_query = "Insert into Subscribers values ($phoneno, '$compid', '$catid', '$pri', '$lim', '$lim')";
+			$subs_res = mysqli_query($conn, $subs_query);
+		}	
+		else if ($_POST["submit"] == "Update!")
+		{
+			$_SESSION["subs_msg"] = "Your subscription to <b> ".$compname." </b> for <b>".$catname."</b> has been updated";
+			$update_query = "Update Subscribers Set Priority='$pri', lim='$lim', currlim='$lim' where PhoneNo='$phoneno' and CompId='$compid' and CatId='$catid'";
+			$update_res = mysqli_query($conn, $update_query);
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Subscribe!</title>
-
-<link rel="stylesheet" type="text/css" href="style.css" />
-<style type="text/css">
-#categories {
-	clear: both;
-	margin-top: 30px;
-	border: 2px solid red;
-	width: 96%;
-	margin-left: 2%;
-	height: 900px;
-}
-.subcategory-block{
-	border: 2px solid #2196F3;
-	width: 95%;
-	float: left;
-	margin: 2%;
-	position: relative;
-	padding-top: 6%;
-	padding-bottom: 1%;
-	text-align: center;
-	font-size: 2.3em;
-	font-weight: bold;
-	color: #2196F3;
-}
-#extra-stuff {
-	font-size: 0.6em;
-	color: black;
-	font-weight: bold;
-	display: inline;
-}
-</style>
+	<title>Subscribe!</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
@@ -59,12 +50,20 @@
 		<li><a class="last-link" href="logout.php">Logout!</a></li>
 	</ul>
 	
-	<div>
-	<br/>
+	<?php if (isset($_SESSION['subs_msg'])) { ?>
+		<p class="big-text"><?php echo $_SESSION["subs_msg"] ?></p>
+	<?php
+		$_SESSION["subs_msg"] = null;
+	
+	} ?>
+
 	<div id="categories">
 	<?php 
-	$cat = $_GET['cat'];
-	$query = "Select * from SubCat where ParCatId = '$cat'";
+	if (isset($_GET['parent_id']))
+		$parent_id = $_GET['parent_id'];
+	else
+		$parent_id = $_POST['parent_id'];
+	$query = "Select * from SubCat where ParCatId = '$parent_id'";
 	$res = mysqli_query($conn, $query);
 	($res ? $count = mysqli_num_rows($res): $count = 0);
 	//$pquery=mysqli_prepare($conn,"Select * from Subscribers where PhoneNo='$phoneno' and CompId=? and CatId=?");
@@ -78,31 +77,32 @@
 			$query = "Select Name, C.CompId from Cat_Comp CC, Company C where C.CompId = CC.CompId and SCat_Id = '$data[1]'";
 			$res2 = mysqli_query($conn, $query);
 			$cnt2 = mysqli_num_rows($res2);
-			?>
 			
-				<?php
-				
-				for($j=0;$j < $cnt2; $j++)
-				{
-					?>
-					<form action="submit.php" method="get">
-				<input type="hidden" name="scat" value="<?php echo $data[1]; ?>" >
+			for($j=0; $j < $cnt2; $j++)
+			{
+			?>
+			<form action="choice.php" method="post" >
 				<?php
 					$rowd = mysqli_fetch_row($res2);
-					$query="Select * from Subscribers where PhoneNo='$phoneno' and CompId='$rowd[1]' and CatId='$data[1]'";
-					$res3=mysqli_query($conn,$query);
-					$cnt3=mysqli_num_rows($res3);
+					$query = "Select * from Subscribers where PhoneNo='$phoneno' and CompId='$rowd[1]' and CatId='$data[1]'";
+					$res3 = mysqli_query($conn, $query);
+					$cnt3 = mysqli_num_rows($res3);
 					
 					//mysqli_stmt_bind_param($pquery,"ii",$rowd[1],$data[1]);
-					echo "<input type=checkbox name=Company[] value=$rowd[1] />&nbsp;".$rowd[0]."       ";
+					echo $rowd[0]."       ";
 				?>
-				<span id="extra-stuff">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Low <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=0 required />
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Medium <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=1 />
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;High <input type="radio" name="<?php echo $rowd[1]."priority" ?>" value=2 /> 
+				<span id="extra-stuff">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				Low <input type="radio" name="pri" value=0 required />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				Medium <input type="radio" name="pri" value=1 checked="checked" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				High <input type="radio" name="pri" value=2 />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enter Limit: <input style="width:50px" type="number" min=0 name="<?php echo $rowd[1]."limit"?>" required />
-				<br/>
+				Enter Limit: <input style="width:50px" type="number" min=0 name="lim" value=5 required />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				</span>
+				<input type="hidden" name="compid" value="<?php echo $rowd[1] ?>" />
+				<input type="hidden" name="compname" value="<?php echo $rowd[0] ?>" />
+				<input type="hidden" name="catid" value="<?php echo $data[1] ?>" />
+				<input type="hidden" name="catname" value="<?php echo $data[2] ?>" />
+				<input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
 				<?php
 				//mysqli_stmt_execute($pquery);
 				/*	if(mysqli_stmt_fetch($pquery))
@@ -111,24 +111,23 @@
 						echo "<input type=hidden name=test value=0>";*/
 				if($cnt3>0)
 				{
-						echo "<input type=hidden name=test value=0>";
-						echo "<input type=submit value='Update!'/><br/>";
-				}
-					else
-					{
-						echo "<input type=hidden name=test value=1>";
-						echo "<input type=submit value='Subscribe!'/><br/>";
-					}
-				echo "</form>";
-				}		
 				?>
-				
-			
-		
+						<input class="submit-button" type="submit" name="submit" value="Update!" /><br/>
+				<?php
+				}
+				else
+				{
+					?>
+					<input class="submit-button" type="submit" name="submit" value="Subscribe!"/><br/>
+				<?php
+				} ?>
+			</form>
+			<?php
+			}		
+			?>
 		</div>
 	<?php
 	}  ?>
-	</div>
 	</div>
 </body>
 </html>
